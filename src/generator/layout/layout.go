84489b6e6200c089764651098187a81157d96736
Moritz Wagner <moritz.wagner@mo-sys.de>
2019-01-05 02:49:17 +0100

    added checksum for data structure, currently unused

--- src/generator/layout/layout.go
+++ src/generator/layout/layout.go
@@ -3,16 +3,27 @@ package layout
 import (
 	"errors"
 	"io"
+	"fmt"
 	"io/ioutil"
+	"crypto/sha512"
 	"gopkg.in/yaml.v1"
+	"encoding/hex"
 )
 
 type Layout struct {
 	Databases []Database
+	CheckSum string
 }
 
 func (*Layout)TypeStrings()map[int]string {
 	return typeStrings;
+}
+
+func (l *Layout)setCheckSum() {
+	d,_ := yaml.Marshal(l)
+	c := sha512.New()
+	c.Write([]byte(d))
+	l.CheckSum = hex.EncodeToString(c.Sum(nil))
 }
 
 func NewLayout(r io.Reader)(*Layout,error) {
@@ -31,5 +42,6 @@ func NewLayout(r io.Reader)(*Layout,error) {
 	if len(l.Databases) == 0 {
 		return nil, errors.New("no databases found")
 	}
+	l.setCheckSum()
 	return l,nil
 }
